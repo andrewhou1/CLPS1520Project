b36f371b00a2c2163128345b3fc7c5762e175793
NP-coder <zhenhao_hou@brown.edu>
2016-12-03 17:21:18 -0500

    Add files via upload

--- randomPixels.py
+++ randomPixels.py
@@ -1,15 +1,15 @@
 from random import randint
 
-def gencoordinates(m, n):
+def gencoordinates(mx, nx, my, ny):
 	seen = set()
-	x, y = randint(m, n), randint(m, n)
+	x, y = randint(mx, nx), randint(my, ny)
 
 	while True:
 		seen.add((x, y))
 		yield(x,y)
-		x, y = randint(m, n), randint(m, n)
+		x, y = randint(mx, nx), randint(my, ny)
 		while (x,y) in seen:
-			x, y = randint(m,n), randint(m,n)
+			x, y = randint(mx,nx), randint(my,ny)
 
-g = gencoordinates(1, 100)
+g = gencoordinates(1, 100, 1, 100)
 print next(g)
